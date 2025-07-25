b3e671e0ca2343113e60c3a43a7cad8aa0e48e00
NGL-ZERO <itsneverfall@gmail.com>
2025-07-25 16:43:14 +0300

    Update mobs.lua

--- mobs.lua
+++ mobs.lua
@@ -1,1 +1,14 @@
-
+return {
+    Slime = {ModelName = "slime", ParentName = "slimeZone", DefaultColor = Color3.fromRGB(0, 255, 0)},
+    SpikeSlime = {ModelName = "slimeSpike", ParentName = "slimeSpikeZone", DefaultColor = Color3.fromRGB(255, 127, 0)},
+    OreMonster = {ModelName = "oreMonster", ParentName = nil, HasSubModel = true, DefaultColor = Color3.fromRGB(0, 127, 255)},
+    SlimeCat = {ModelName = "slimeCat", ParentName = "slimeCatZone", DefaultColor = Color3.fromRGB(179, 42, 172)},
+    Turret = {ModelName = "Turret", ParentName = "turret", DefaultColor = Color3.fromRGB(100, 100, 100)},
+    SlimeVoid = {ModelName = "slimeVoid", ParentName = "slimeVoidZone", DefaultColor = Color3.fromRGB(0, 1, 87)},
+    Crab = {ModelName = "crab", ParentName = "crabZone", DefaultColor = Color3.fromRGB(62, 63, 95)},
+    CrushHead = {ModelName = "crushHead", ParentName = "crushHeadZone", DefaultColor = Color3.fromRGB(255, 255, 255)},
+    Log = {ModelName = "log", ParentName = "golemBossFight", DefaultColor = Color3.fromRGB(255, 255, 255)},
+    Hollow = {ModelName = "hollow", ParentName = "hollowHeadZone", DefaultColor = Color3.fromRGB(255, 255, 255)},
+    AraxiaTheFallenLord = {ModelName = "araxiaTheFallenLord", ParentName = "araxiaBossFight", DefaultColor = Color3.fromRGB(255, 255, 255)},
+    AraxiumSkelly = {ModelName = "AraxiumSkelly", ParentName = "AraxiumSkellyZone", DefaultColor = Color3.fromRGB(255, 255, 255)},
+}
